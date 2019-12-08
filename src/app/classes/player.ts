f2a6e1cd953e65b465584f83e6b4b0eba6a83c5d
Dave Mickelsen <iamtheweasel@gmail.com>
2019-12-07 23:43:56 -0700

    Added position placeholders for each player's dice, started adding logic to move die to placeholders.

--- src/app/classes/player.ts
+++ src/app/classes/player.ts
@@ -1,5 +1,9 @@
+import { ElementRef } from '@angular/core';
+
 export class Player {
     public name: string;
     public totalScore: number = 0;
     public currentRoundScore: number = 0;
+    public hasTurn: boolean;
+    public savedDice: ElementRef[]
 }
--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, Renderer2 } from '@angular/core';
 import { Die } from 'src/app/classes/die';
 import { Player } from 'src/app/classes/player';
 
@@ -7,7 +7,7 @@ import { Player } from 'src/app/classes/player';
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent implements OnInit {
+export class GameComponent implements OnInit, AfterViewInit {
 
   playerCount: number = 4;
   playerList: Player[];
@@ -15,30 +15,58 @@ export class GameComponent implements OnInit {
   player1: Player = {
     name: 'Luke',
     totalScore: 0,
-    currentRoundScore: 0
+    currentRoundScore: 0,
+    hasTurn: false,
+    savedDice: []
   }
 
   player2: Player = {
     name: 'Vader',
     totalScore: 0,
-    currentRoundScore: 0
+    currentRoundScore: 0,
+    hasTurn: false,
+    savedDice: []
   }
 
   player3: Player = {
     name: 'Yoda',
     totalScore: 0,
-    currentRoundScore: 0
+    currentRoundScore: 0,
+    hasTurn: false,
+    savedDice: []
   }
 
   player4: Player = {
     name: 'Palpatine',
     totalScore: 0,
-    currentRoundScore: 0
+    currentRoundScore: 0,
+    hasTurn: false,
+    savedDice: []
   }
 
   public diceList: Die[];
   public diceToRollList: Die[];
   public savedDiceList: Die[];
+
+  @ViewChild('player1D1Pos', {static: false}) player1Die1Pos: ElementRef;
+  @ViewChild('player1D2Pos', {static: false}) player1Die2Pos: ElementRef;
+  @ViewChild('player1D3Pos', {static: false}) player1Die3Pos: ElementRef;
+  @ViewChild('player1D4Pos', {static: false}) player1Die4Pos: ElementRef;
+
+  @ViewChild('player2D1Pos', {static: false}) player2Die1Pos: ElementRef;
+  @ViewChild('player2D2Pos', {static: false}) player2Die2Pos: ElementRef;
+  @ViewChild('player2D3Pos', {static: false}) player2Die3Pos: ElementRef;
+  @ViewChild('player2D4Pos', {static: false}) player2Die4Pos: ElementRef;
+
+  @ViewChild('player3D1Pos', {static: false}) player3Die1Pos: ElementRef;
+  @ViewChild('player3D2Pos', {static: false}) player3Die2Pos: ElementRef;
+  @ViewChild('player3D3Pos', {static: false}) player3Die3Pos: ElementRef;
+  @ViewChild('player3D4Pos', {static: false}) player3Die4Pos: ElementRef;
+
+  @ViewChild('player4D1Pos', {static: false}) player4Die1Pos: ElementRef;
+  @ViewChild('player4D2Pos', {static: false}) player4Die2Pos: ElementRef;
+  @ViewChild('player4D3Pos', {static: false}) player4Die3Pos: ElementRef;
+  @ViewChild('player4D4Pos', {static: false}) player4Die4Pos: ElementRef;
 
   die1: Die = {
     sides: 6,
@@ -70,7 +98,7 @@ export class GameComponent implements OnInit {
 
   testValues: string;
 
-  constructor() { }
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit() {
     this.playerList = [
@@ -89,6 +117,19 @@ export class GameComponent implements OnInit {
     this.diceToRollList = this.diceList;
   }
 
+  ngAfterViewInit() {
+    console.log(this.player1Die1Pos.nativeElement.offsetLeft);
+    console.log(this.player1Die1Pos.nativeElement.offsetTop);
+    // setTimeout(() => {
+    //   this.renderer.setStyle(this.player1Die1Pos.nativeElement, 'position', 'absolute');
+    //   let newTop = this.player1Die1Pos.nativeElement.offsetTop + 30;
+    //   let newLeft = this.player1Die1Pos.nativeElement.offsetLeft + 30;
+    //   console.log(newTop);
+    //   this.renderer.setStyle(this.player1Die1Pos.nativeElement, 'top', String(newTop) + 'px');
+    //   this.renderer.setStyle(this.player1Die1Pos.nativeElement, 'left', String(newLeft) + 'px');
+    // }, 2000)
+  }
+
   rollDice(): void {
     this.diceToRollList.forEach(die => {
       die.value = Math.floor(Math.random() * Math.floor(die.sides)) + 1;
@@ -96,6 +137,9 @@ export class GameComponent implements OnInit {
     });
   }
 
-  
+  getPlayersDicePositions(): void {
+
+  }
+
 
 }
